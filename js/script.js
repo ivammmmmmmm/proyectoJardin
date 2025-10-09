ac7fb75dd56c41d9b0f61aa3d86bec29e51bd83f
Maximo-22 <segundomaximo96@gmail.com>
2025-10-09 15:11:34 -0300

    ajax sigue sin funcionar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-function alumnosAcordeon(){
+$(document).ready(function (){
     $.ajax({
         url: '../php/modificarAlumno.php',
         type: 'POST',
-
+        dataType:'json',
+        
         success: function(respuesta){
             $('#alumnosLista').html(respuesta);
+        },
+
+        error: function(xhr, status, error){
+                // La función se ejecuta si hay un error
+                console.error("Error al obtener los datos: " + error);
+                $('#contenedor-registros').html('<p>Ocurrió un error al cargar los datos.</p>');
         }
     })
-}
+});
