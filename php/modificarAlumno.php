11100e7d8754b4936384b7017f896bc8378c0f96
ivammmmmmmm <ivan9dertolotti@gmail.com>
2025-10-29 18:34:25 -0300

    arreglos en modificar alumno

--- php/modificarAlumno.php
+++ php/modificarAlumno.php
@@ -2,7 +2,7 @@
 //
 //FIXME
 require "conexion.php";
-$columnas= ['alumno.id', 'alumno.nombre', 'alumno.apellido', 'alumno.dni', 'alumno.direccion', 'alumno.fecha_nacimiento', 'estado.nombre as nombreestado'];
+$columnas= ['alumno.id', 'alumno.nombre', 'alumno.apellido', 'alumno.dni', 'alumno.direccion', 'alumno.fecha_nacimiento as fechanacimiento', 'estado.nombre as nombreestado'];
 $tabla="alumno";
 $campo = isset($_POST['campo']) ? $conexion->real_escape_string($_POST['campo']) : null;
 
@@ -28,7 +28,7 @@ $html = '';
 
 if ($num_rows > 0) {
     while ($row = $resultado->fetch_assoc()) {
-        $edad = date_diff(date_create($row['alumno.fecha_nacimiento']), date_create('today'))->y;
+        $edad = date_diff(date_create($row['fechanacimiento']), date_create('today'))->y;
 
         $html .= '<div class="list-group-item bg-dark text-light">';
         $html .= '  <div class="d-flex flex-column flex-sm-row justify-content-between align-items-start align-items-sm-center gap-3">';
@@ -47,7 +47,7 @@ if ($num_rows > 0) {
         $html .= '      <p><strong>ID:</strong> '.$row['id'].'</p>';
         $html .= '      <p><strong>DNI:</strong> '.$row['dni'].'</p>';
         $html .= '      <p><strong>Edad:</strong> '.$edad.'</p>';
-        $html .= '      <p><strong>Fecha de Nacimiento:</strong> '.$row['fecha_nacimiento'].'</p>';
+        $html .= '      <p><strong>Fecha de Nacimiento:</strong> '.$row['fechanacimiento'].'</p>';
         $html .= '      <p><strong>Dirección:</strong> '.$row['direccion'].'</p>';
         $html .= '      <p><strong>Estado:</strong> '.$row['nombreestado'].'</p>';
         $html .= '    </div>';
