d82fcec77e7757e56e31fc40a883f7a0827ccd58
Maximo-22 <segundomaximo96@gmail.com>
2025-11-14 02:33:19 -0300

    Add files via upload

--- sql/proyecto_jardin.sql
+++ sql/proyecto_jardin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2025 a las 00:16:36
+-- Tiempo de generación: 14-11-2025 a las 06:31:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -35,27 +35,28 @@ CREATE TABLE `alumno` (
   `direccion` varchar(25) NOT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
   `idLocalidad` int(11) DEFAULT NULL,
-  `idSala` int(11) NOT NULL
+  `idSala` int(11) NOT NULL,
+  `idEstado` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `alumno`
 --
 
-INSERT INTO `alumno` (`id`, `nombre`, `apellido`, `dni`, `direccion`, `fecha_nacimiento`, `idLocalidad`, `idSala`) VALUES
-(6, 'a', 's', 2, 'micasa', '0000-00-00', NULL, 1),
-(7, 'a', 's', 2, 'micasa', '2025-08-12', NULL, 2),
-(8, 'a', 's', 2, 'micasa', '2025-08-12', NULL, 3),
-(9, 'alejo', 'cangrejo', 48863144, 'bolivia 223', '2058-03-22', NULL, 3),
-(16, 'geronimo', 'corleta', 90, 'cerrito', '1987-02-22', NULL, 2),
-(17, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 1),
-(18, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 3),
-(19, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 2),
-(20, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 2),
-(21, 'Jose', 'Golpe', 48926736, 'El callao 9288', '2006-08-17', NULL, 1),
-(22, 'a', 's', 0, 'sasa', '1231-02-22', 663804012, 1),
-(23, 'asd', 'asd', 213, 'sadas', '1111-11-11', 2147483647, 1),
-(24, 'a', 'b', 2, '11', '0000-00-00', 34028010, 1);
+INSERT INTO `alumno` (`id`, `nombre`, `apellido`, `dni`, `direccion`, `fecha_nacimiento`, `idLocalidad`, `idSala`, `idEstado`) VALUES
+(6, 'a', 's', 2, 'micasa', '0000-00-00', NULL, 1, NULL),
+(7, 'a', 's', 2, 'micasa', '2025-08-12', NULL, 2, NULL),
+(8, 'a', 's', 2, 'micasa', '2025-08-12', NULL, 3, NULL),
+(9, 'alejo', 'cangrejo', 48863144, 'bolivia 223', '2058-03-22', NULL, 3, NULL),
+(16, 'geronimo', 'corleta', 90, 'cerrito', '1987-02-22', NULL, 2, NULL),
+(17, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 1, NULL),
+(18, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 3, NULL),
+(19, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 2, NULL),
+(20, 'juan', 'ponce', 48853140, 'cerrito2', '2000-03-20', NULL, 2, NULL),
+(21, 'Jose', 'Golpe', 48926736, 'El callao 9288', '2006-08-17', NULL, 1, NULL),
+(22, 'a', 's', 0, 'sasa', '1231-02-22', 663804012, 1, NULL),
+(23, 'asd', 'asd', 213, 'sadas', '1111-11-11', 2147483647, 1, NULL),
+(24, 'a', 'b', 2, '11', '0000-00-00', 34028010, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -96,8 +97,8 @@ CREATE TABLE `comunicado` (
   `idSala` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `medioUtilizado` varchar(25) NOT NULL,
-  `causa` varchar(255) NOT NULL,
-  `desarrollo` varchar(255) NOT NULL
+  `causa` varchar(25) NOT NULL,
+  `desarrollo` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -146,6 +147,16 @@ CREATE TABLE `estado` (
   `nombre` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`id`, `nombre`) VALUES
+(1, 'Cursando'),
+(2, 'Dado de baja'),
+(3, 'promocionado'),
+(4, 'egresado');
+
 -- --------------------------------------------------------
 
 --
@@ -165,7 +176,35 @@ CREATE TABLE `falta` (
 --
 
 INSERT INTO `falta` (`id`, `idAlumno`, `idSala`, `idRazon`, `fecha`) VALUES
-(52, 16, 2, 1, '2025-11-11');
+(3, 18, 2, 4, '2025-10-17'),
+(4, 19, 2, 3, '2025-10-18'),
+(5, 20, 3, 1, '2025-10-19'),
+(6, 21, 3, 5, '2025-10-20'),
+(7, 24, 2, 2, '2025-10-21'),
+(8, 16, 2, 3, '2025-10-22'),
+(9, 19, 2, 3, '2025-10-22'),
+(10, 20, 2, 3, '2025-10-22'),
+(11, 7, 2, 3, '2025-10-22'),
+(12, 23, 1, 3, '2025-10-22'),
+(13, 24, 1, 3, '2025-10-22'),
+(14, 21, 1, 3, '2025-10-22'),
+(15, 17, 1, 3, '2025-10-22'),
+(16, 6, 1, 3, '2025-10-22'),
+(17, 22, 1, 3, '2025-10-22'),
+(18, 9, 3, 1, '2025-10-23'),
+(19, 23, 1, 4, '2025-10-27'),
+(20, 23, 1, 1, '2025-10-28'),
+(21, 16, 1, 2, '2025-11-09'),
+(22, 16, 1, 2, '2025-11-10'),
+(23, 16, 1, 3, '2025-11-12'),
+(52, 16, 1, 2, '2025-11-11'),
+(53, 16, 1, 2, '2025-11-13'),
+(54, 16, 2, 1, '2025-11-13'),
+(55, 16, 2, 3, '2025-11-13'),
+(56, 16, 2, 1, '2025-11-14'),
+(57, 19, 2, 2, '2025-11-14'),
+(58, 20, 2, 1, '2025-11-14'),
+(59, 7, 2, 3, '2025-11-14');
 
 -- --------------------------------------------------------
 
@@ -200,7 +239,7 @@ CREATE TABLE `padretutor` (
 --
 
 INSERT INTO `padretutor` (`id`, `nombre`, `apellido`, `telefono`, `direccion`, `idLocalidad`, `mail`, `dni`) VALUES
-(3, 'a', 's', 11, 'sad', 0, NULL, NULL),
+(3, 'a', 's', 11, '', 0, NULL, NULL),
 (4, 'a', 's', 11, NULL, 0, NULL, NULL),
 (5, 'a', 's', 11, NULL, 0, NULL, NULL),
 (6, 'a', 's', 11, NULL, 0, NULL, NULL),
@@ -240,7 +279,19 @@ INSERT INTO `presente` (`id`, `idAlumno`, `idSala`, `fecha`) VALUES
 (258, 21, 1, '2025-11-11'),
 (259, 17, 1, '2025-11-11'),
 (260, 6, 1, '2025-11-11'),
-(261, 22, 1, '2025-11-11');
+(261, 22, 1, '2025-11-11'),
+(263, 19, 2, '2025-11-13'),
+(264, 20, 2, '2025-11-13'),
+(265, 7, 2, '2025-11-13'),
+(268, 21, 1, '2025-11-13'),
+(269, 17, 1, '2025-11-13'),
+(270, 6, 1, '2025-11-13'),
+(271, 22, 1, '2025-11-13'),
+(273, 18, 3, '2025-11-13'),
+(274, 8, 3, '2025-11-13'),
+(275, 19, 2, '2025-11-14'),
+(276, 20, 2, '2025-11-14'),
+(277, 7, 2, '2025-11-14');
 
 -- --------------------------------------------------------
 
@@ -413,13 +464,13 @@ ALTER TABLE `docente`
 -- AUTO_INCREMENT de la tabla `estado`
 --
 ALTER TABLE `estado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `falta`
 --
 ALTER TABLE `falta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT de la tabla `localidad`
@@ -437,7 +488,7 @@ ALTER TABLE `padretutor`
 -- AUTO_INCREMENT de la tabla `presente`
 --
 ALTER TABLE `presente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=262;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=278;
 
 --
 -- AUTO_INCREMENT de la tabla `razon`
